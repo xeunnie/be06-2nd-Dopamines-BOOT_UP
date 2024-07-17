09cf195bfaba1f64b8526658531974b691504bae
subin <starbin7788@gmail.com>
2024-07-18 01:07:34 +0900

    [Feat] 자유 게시판 댓글 좋아요 기능 구현

--- src/main/java/com/example/dopamines/domain/board/community/free/controller/FreeLikeController.java
+++ src/main/java/com/example/dopamines/domain/board/community/free/controller/FreeLikeController.java
@@ -1,6 +1,5 @@
 package com.example.dopamines.domain.board.community.free.controller;
 
-import com.example.dopamines.domain.board.community.free.model.response.FreeLikeRes;
 import com.example.dopamines.domain.board.community.free.service.FreeLikeService;
 import com.example.dopamines.domain.user.model.entity.User;
 import com.example.dopamines.global.common.BaseResponse;
@@ -18,13 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class FreeLikeController {
     private final FreeLikeService freeLikeService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<BaseResponse<?>> create(@AuthenticationPrincipal CustomUserDetails customUserDetails, Long idx){
+    @RequestMapping(method = RequestMethod.GET, value = "/free-board")
+    public ResponseEntity<BaseResponse<?>> createFreeBoardLike(@AuthenticationPrincipal CustomUserDetails customUserDetails, Long idx){
         User user = customUserDetails.getUser();
-        String result = freeLikeService.create(user,idx);
-        return ResponseEntity.ok(new BaseResponse<>(result));
+        String result = freeLikeService.createFreeBoardLike(user,idx);
 
+        return ResponseEntity.ok(new BaseResponse<>(result));
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/comment")
+    public ResponseEntity<BaseResponse<?>> createCommentLike(@AuthenticationPrincipal CustomUserDetails customUserDetails, Long idx){
+        User user = customUserDetails.getUser();
+        String result = freeLikeService.createCommentLike(user,idx);
 
+        return ResponseEntity.ok(new BaseResponse<>(result));
+    }
 }
--- src/main/java/com/example/dopamines/domain/board/community/free/service/FreeLikeService.java
+++ src/main/java/com/example/dopamines/domain/board/community/free/service/FreeLikeService.java
@@ -1,29 +1,33 @@
 package com.example.dopamines.domain.board.community.free.service;
 
 import com.example.dopamines.domain.board.community.free.model.entity.FreeBoard;
+import com.example.dopamines.domain.board.community.free.model.entity.FreeComment;
+import com.example.dopamines.domain.board.community.free.model.entity.FreeCommentLike;
 import com.example.dopamines.domain.board.community.free.model.entity.FreeLike;
-import com.example.dopamines.domain.board.community.free.model.response.FreeLikeRes;
 import com.example.dopamines.domain.board.community.free.repository.FreeBoardRepository;
+import com.example.dopamines.domain.board.community.free.repository.FreeCommentLikeRepository;
+import com.example.dopamines.domain.board.community.free.repository.FreeCommentRepository;
 import com.example.dopamines.domain.board.community.free.repository.FreeLikeRepository;
 import com.example.dopamines.domain.user.model.entity.User;
 import com.example.dopamines.global.common.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
-
 import java.util.Optional;
 
 import static com.example.dopamines.global.common.BaseResponseStatus.COMMUNITY_BOARD_NOT_FOUND;
+import static com.example.dopamines.global.common.BaseResponseStatus.COMMUNITY_COMMENT_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class FreeLikeService {
     private final FreeLikeRepository freeLikeRepository;
     private final FreeBoardRepository freeBoardRepository;
+    private final FreeCommentLikeRepository freeCommentLikeRepository;
+    private final FreeCommentRepository freeCommentRepository;
 
 
-    public String create(User user,Long idx) {
+    public String createFreeBoardLike(User user, Long idx) {
         Optional<FreeLike> result = freeLikeRepository.findByUserAndFreeBoard(user.getIdx(),idx);
         FreeLike freeLike;
 
@@ -42,4 +46,22 @@ public class FreeLikeService {
         return "자유 게시글 좋아요 등록";
     }
 
+    public String createCommentLike(User user, Long idx) {
+        Optional<FreeCommentLike> result = freeCommentLikeRepository.findByUserAndIdx(user.getIdx(),idx);
+        FreeCommentLike freeCommentLike;
+
+        if(result.isPresent()){ // 이미 좋아요한 경우
+            freeCommentLike= result.get();
+            freeCommentLikeRepository.delete(freeCommentLike);
+            return "자유 게시글 댓글 좋아요 취소";
+        }
+
+        FreeComment freeComment = freeCommentRepository.findById(idx).orElseThrow(() -> new BaseException(COMMUNITY_COMMENT_NOT_FOUND));
+        freeCommentLike = FreeCommentLike.builder()
+                .user(user)
+                .freeComment(freeComment)
+                .build();
+        freeCommentLikeRepository.save(freeCommentLike);
+        return "자유 게시글 댓글 좋아요 등록";
+    }
 }

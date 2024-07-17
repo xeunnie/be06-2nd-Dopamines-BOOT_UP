58f14870caac96592dab31daef94d6f4fc09222e
subin <starbin7788@gmail.com>
2024-07-18 00:26:20 +0900

    [Feat] 자유 게시판 댓글 에러 컨벤션 구현

--- src/main/java/com/example/dopamines/global/common/BaseResponseStatus.java
+++ src/main/java/com/example/dopamines/global/common/BaseResponseStatus.java
@@ -34,7 +34,7 @@ public enum BaseResponseStatus {
     COMMUNITY_TITLE_NOT_FOUND(false,4001, "제목을 작성해주세요."),
     COMMUNITY_CONTENT_NOT_FOUND(false,4002, "내용을 작성해주세요."),
     COMMUNITY_USER_NOT_AUTHOR(false,4003, "작성자만이 권한이 있습니다."),
-
+    COMMUNITY_COMMENT_NOT_FOUND(false, 4004, "해당하는 댓글을 찾을 수 없습니다."),
     /**
      * 4200 : MARKET 에러 - 송연
      */

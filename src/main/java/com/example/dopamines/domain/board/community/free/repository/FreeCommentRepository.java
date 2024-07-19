f19297087c8c53c19e1bc03afde04fdcb6feadbb
syy0O <songie05@naver.com>
2024-07-19 17:15:14 +0900

    [Refactor] 댓글 조회 기능 성능 개선
    n+1 문제 해결

--- src/main/java/com/example/dopamines/domain/board/community/free/repository/FreeCommentRepository.java
+++ src/main/java/com/example/dopamines/domain/board/community/free/repository/FreeCommentRepository.java
@@ -1,7 +1,15 @@
 package com.example.dopamines.domain.board.community.free.repository;
 
 import com.example.dopamines.domain.board.community.free.model.entity.FreeComment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FreeCommentRepository extends JpaRepository<FreeComment,Long> {
+    //@Query("SELECT fc FROM FreeComment fc JOIN FETCH fc.user u JOIN FETCH fc.likes l WHERE fc.freePost.idx = :postIdx")
+    @Query("SELECT fc FROM FreeComment fc  WHERE fc.freePost.idx = :postIdx")
+    Slice<FreeComment> findAllWithPaging(Pageable pageable, Long postIdx); // 페이지 사이즈가 10으로 고정
+
+
 }

4744c079bc1b1a54e3546e41faef905f7d0decc5
subin <starbin7788@gmail.com>
2024-07-19 15:49:04 +0900

    [Feat] 자유 게시판 검색 기능 구현

--- src/main/java/com/example/dopamines/domain/board/community/free/repository/FreePostRepository.java
+++ src/main/java/com/example/dopamines/domain/board/community/free/repository/FreePostRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FreePostRepository extends JpaRepository<FreePost, Long> {
     @Query("SELECT f FROM FreePost f")
-    public Slice<FreePost> findAllWithPaging(Pageable pageable);
-//    @Query("SELECT f FROM FreePost f")
-//    Slice<FreePost> search(Pageable pageable, String keyword);
+    Slice<FreePost> findAllWithPaging(Pageable pageable);
+
+    @Query("SELECT f FROM FreePost f WHERE LOCATE(:keyword, f.title) > 0 OR LOCATE(:keyword, f.content) > 0")
+    Slice<FreePost> search(Pageable pageable, @Param("keyword") String keyword);
+
 }

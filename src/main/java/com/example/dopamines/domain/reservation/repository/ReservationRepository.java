11c8a66afda1dd3fecfc86bcc45be90babb64a66
SihyunSeo <tjtlgus4@naver.com>
2024-08-05 17:14:49 +0900

    [hotfix] reservation 로직 변경

--- src/main/java/com/example/dopamines/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/dopamines/domain/reservation/repository/ReservationRepository.java
@@ -1,10 +1,13 @@
 package com.example.dopamines.domain.reservation.repository;
 
 import com.example.dopamines.domain.reservation.model.entity.Reservation;
+import com.example.dopamines.domain.reservation.model.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import javax.swing.text.html.Option;
 import java.nio.ByteBuffer;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     Optional<List<Reservation>> findByUserIdx(Long userIdx);
 
     Optional<Reservation> findBySeatIdx(Long seatIdx);
+
+    // JPQL 쿼리 사용
+    @Query("SELECT r FROM Reservation r WHERE r.seat IN :seatIdxs AND DATE(r.createdAt) = :today")
+    List<Reservation> findBySeatIdxInAndToday(List<Seat> seatIdxs, LocalDate today);
 }

8addd63f668e43963c8501c50dba479816d3108e
SihyunSeo <tjtlgus4@naver.com>
2024-08-11 04:00:49 +0900

    [Fix] Edit Structure - Community

--- src/main/java/com/example/dopamines/domain/board/community/open/model/entity/OpenRecomment.java
+++ src/main/java/com/example/dopamines/domain/board/community/open/model/entity/OpenRecomment.java
@@ -6,7 +6,10 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
+@DynamicInsert
 @Entity
 @Getter
 @Setter
@@ -30,7 +33,19 @@ public class OpenRecomment {
     @JoinColumn(name = "comment_idx")
     private OpenComment openComment;
 
-    @OneToMany(mappedBy = "openRecomment")
-    private List<OpenRecommentLike> likes;
+    //    @OneToMany(mappedBy = "openRecomment")
+//    private List<OpenRecommentLike> likes;
+    @ColumnDefault(value = "0")
+    private Integer likesCount;
 
+    @Version    // 낙관적 락 : 프로그램 단에서 사용하는 Lock, 낙관적 락 테스트할 때는 비관적 락 주석 처리
+    @ColumnDefault(value = "0")
+    private Integer version;
+
+    public void addLikesCount() {
+        this.likesCount = this.likesCount + 1;
+    }
+    public void subLikesCount() {
+        this.likesCount = this.likesCount - 1;
+    }
 }

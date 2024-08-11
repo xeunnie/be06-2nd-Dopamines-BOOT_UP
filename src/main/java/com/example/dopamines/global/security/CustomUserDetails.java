3cde7276de7f95303c987ee19a8861aa32b05ec0
706com <ehdgusjdbswl@gmail.com>
2024-08-11 23:48:11 +0900

    [fix] 로그인 불가 이슈 해결

--- src/main/java/com/example/dopamines/global/security/CustomUserDetails.java
+++ src/main/java/com/example/dopamines/global/security/CustomUserDetails.java
@@ -35,14 +35,14 @@ public class CustomUserDetails implements UserDetails {
         return user.getEmail();
     }
 
-    //이메일 인증 실패하거나,회원 정지 상태면 로그인 및 jwt 발급 실패
-    @Override
-    public boolean isEnabled() {
-        if(user.isEnabled() && user.isStatus()) {
-            return user.isEnabled();
-        }
-        return false;
-    }
+//    //이메일 인증 실패하거나,회원 정지 상태면 로그인 및 jwt 발급 실패
+//    @Override
+//    public boolean isEnabled() {
+//        if(user.isEnabled() && user.isStatus()) {
+//            return user.isEnabled();
+//        }
+//        return false;
+//    }
 
     public Long getIdx() {return user.getIdx();}
 
